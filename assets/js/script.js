029d718f536c980db79e9bbdf968df41a514ce0b
alextgco <alextgco@gmail.com>
2017-04-22 23:30:47 +0300

    main

--- assets/js/script.js
+++ assets/js/script.js
@@ -10,5 +10,52 @@ $(document).ready(function(){
 
     });
 
+    $('.e-search-param-box').off('click').on('click', function(){
+
+        $('.e-search-block').not($(this).parents('.e-search-block')).removeClass('opened');
+        $(this).parents('.e-search-block').toggleClass('opened');
+
+    });
+
+
+    $('.venue-images-dot').off('click').on('click', function () {
+
+        if($(this).hasClass('active')){
+            return;
+        }
+
+        var self  =$(this);
+        var idx = $(this).index();
+        var p = $(this).parents('.venue-images-box');
+        var t = p.find('.venue-images-train');
+
+        t.animate({
+            marginLeft: - (idx * 300) + 'px'
+        }, 220, function () {
+
+            p.find('.venue-images-dot').removeClass('active');
+            self.addClass('active');
+
+        });
+
+    });
+
+    $('.e-search-pick-param').off('click').on('click', function(){
+
+        $(this).toggleClass('checked');
+
+    });
+
+    $('.map-search-extended-toggler').off('click').on('click', function(){
+
+        $(this).toggleClass('opened');
+        if($(this).hasClass('opened')){
+            $(this).html('Скрыть расширенный поиск');
+        }else{
+            $(this).html('Расширенный поиск');
+        }
+        $('.map-search-extended').toggleClass('opened');
+
+    });
 
 });
